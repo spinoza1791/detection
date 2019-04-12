e4aca5c4b10a15d58693da4f28a3fd45d54ce39c
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-12 16:35:26 -0500

    Update fast-pi3d-cam-orig.py

--- fast-pi3d-cam-orig.py
+++ fast-pi3d-cam-orig.py
@@ -100,7 +100,7 @@ while not new_pic:
     time.sleep(0.1)
 
 ########################################################################
-DISPLAY = pi3d.Display.create(preview_mid_X, preview_mid_Y, w=preview_W, h=preview_H, layer=1, frames_per_second=max_fps)
+DISPLAY = pi3d.Display.create(preview_mid_X, preview_mid_Y, w=preview_W, h=preview_H, layer=0, frames_per_second=max_fps)
 DISPLAY.set_background(0.0, 0.0, 0.0, 0.0)
 shader = pi3d.Shader("uv_reflect")
 flatsh = pi3d.Shader('uv_flat')
