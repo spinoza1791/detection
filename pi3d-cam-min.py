aecca9b53fed54c98bc4c508fd344a1b7f087477
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-12 13:21:04 -0500

    Update pi3d-cam-min.py

--- pi3d-cam-min.py
+++ pi3d-cam-min.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 """ Example showing what can be left out. ESC to quit"""
-import demo
+#import demo
 import pi3d
 import numpy as np
 import picamera
