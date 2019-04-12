dada69c1f826ff5c1464374ec8c51909505632e6
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-12 18:45:25 -0500

    Update fast-pi3d-cam-orig.py

--- fast-pi3d-cam-orig.py
+++ fast-pi3d-cam-orig.py
@@ -67,8 +67,8 @@ class ImageProcessor(threading.Thread):
           if self.stream.tell() >= NBYTES:
             self.stream.seek(0)
             g_input = np.frombuffer(self.stream.getvalue(), dtype=np.uint8)
-            g_input = (g_input * [0.2989, 0.5870, 0.1140]).sum(axis=2).astype(np.uint8)
             bnp = np.array(self.stream.getbuffer(), dtype=np.uint8).reshape(CAMH, CAMW, 3)
+            bnp = (bnp * [0.2989, 0.5870, 0.1140]).sum(axis=2).astype(np.uint8)
             npa[:,:,0:3] = bnp         
             new_pic = True
         except Exception as e:
