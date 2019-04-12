87f32cc39da071e10927dfd599d7559e198f94c2
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-12 13:15:21 -0500

    Update pi3d-threaded-cam.py

--- pi3d-threaded-cam.py
+++ pi3d-threaded-cam.py
@@ -34,7 +34,7 @@ preview_mid_X = int(screen_W/2 - preview_W/2)
 preview_mid_Y = int(screen_H/2 - preview_H/2)
 
 max_obj = 5
-max_fps = 25
+max_fps = 30
 
 DISPLAY = pi3d.Display.create(preview_mid_X, preview_mid_Y, w=preview_W, h=preview_H, layer=1, frames_per_second=max_fps)
 DISPLAY.set_background(0.0, 0.0, 0.0, 0.0) # transparent
