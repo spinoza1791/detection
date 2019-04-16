0f5ad20dbc9178dff34d2d6b2afc928d3a4da7c7
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-16 12:15:33 -0500

    Update pi-tpu.py

--- pi-tpu.py
+++ pi-tpu.py
@@ -107,16 +107,20 @@ def main():
 	fps = "00.0 fps"
 	N = 10
 	ms = "00"
+	#screen = pygame.display.get_surface() #get the surface of the current active display
+	#resized_x,resized_y = size = screen.get_width(), screen.get_height()
+	img = pycam.get_image()
+	img = pygame.transform.scale(img,(mdl_dims,mdl_dims))
 	
 	while True:
-		screen = pygame.display.get_surface() #get the surface of the current active display
-		resized_x,resized_y = size = screen.get_width(), screen.get_height()
+		#screen = pygame.display.get_surface() #get the surface of the current active display
+		#resized_x,resized_y = size = screen.get_width(), screen.get_height()
 		img = pycam.get_image()
-		img = pygame.transform.scale(img,(resized_x, resized_y))
+		#img = pygame.transform.scale(img,(resized_x, resized_y))
 		#if img and video_off == False:
 			#screen.blit(img, (0,0))
 					
-		img = pygame.transform.scale(img,(mdl_dims,mdl_dims))
+		#img = pygame.transform.scale(img,(mdl_dims,mdl_dims))
 		img_arr = pygame.surfarray.pixels3d(img)
 		#if gray:
 		#	img_arr = grayscale(img)
