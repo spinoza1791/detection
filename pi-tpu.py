6c436a6cefcc42b004df21abfe77f29380a31989
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-16 12:05:21 -0500

    Update pi-tpu.py

--- pi-tpu.py
+++ pi-tpu.py
@@ -126,15 +126,13 @@ def main():
 			#print(img_arr.shape)
 			#print(img_arr.size)
 			
-		#img_arr = np.swapaxes(img_arr,0,1)
-		#img_arr = np.ascontiguousarray(img_arr)
-		#frame = io.BytesIO(img_arr)
-		#frame_buf_val = np.frombuffer(frame.getvalue(), dtype=np.uint8)
+		img_arr = np.swapaxes(img_arr,0,1)
+		img_arr = np.ascontiguousarray(img_arr)
+		frame = io.BytesIO(img_arr)
+		frame_buf_val = np.frombuffer(frame.getvalue(), dtype=np.uint8)
 		#print(frame_buf_val)
 		start_ms = time.time()
-		img_arr = pygame.surfarray.array2d(img)
-		results = engine.DetectWithImage(img_arr, threshold=thresh, keep_aspect_ratio=False, relative_coord=False, top_k=max_obj)
-		#results = engine.DetectWithInputTensor(frame_buf_val, threshold=thresh, top_k=max_obj)
+		results = engine.DetectWithInputTensor(frame_buf_val, threshold=thresh, top_k=max_obj)
 		elapsed_ms = time.time() - start_ms
 		#pygame.surfarray.blit_array(screen, img_arr)	
 		i += 1
