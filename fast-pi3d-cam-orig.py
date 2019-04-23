30f05762b01ff476862307e18940cdca64efa221
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-23 14:21:31 -0500

    Update fast-pi3d-cam-orig.py

--- fast-pi3d-cam-orig.py
+++ fast-pi3d-cam-orig.py
@@ -171,8 +171,10 @@ while DISPLAY.loop_running():
     ms_avg = round(ms_total / (N + 1))
     ms_str = str(ms_avg)
     ms_txt.quick_change(ms_str)
-    if ms_avg > 0:
-      camera.framerate = round(1000 / ms_avg)
+    max_cam = round(1000 / ms_avg)
+    print("cam rate:" +  max_cam)
+    if max_cam > 20:
+      camera.framerate = max_cam
     ms_total = 0
     
   if new_pic:
