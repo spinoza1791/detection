a3ddde20a0131dd7030917dd7fcf629b35feb56a
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-27 19:12:40 -0500

    Update pi-tpu-dev.py

--- pi-tpu-dev.py
+++ pi-tpu-dev.py
@@ -73,47 +73,47 @@ def main():
 	engine = edgetpu.detection.engine.DetectionEngine(args.model)
 	
 	class PiVideoStream:
-	global mdl_dims
-	def __init__(self, resolution=(mdl_dims, mdl_dims), framerate=32):
-		# initialize the camera and stream
-		self.camera = PiCamera()
-		self.camera.resolution = resolution
-		self.camera.framerate = framerate
-		self.rawCapture = PiRGBArray(self.camera, size=resolution)
-		self.stream = self.camera.capture_continuous(self.rawCapture,
-			format="bgr", use_video_port=True)
- 
-		# initialize the frame and the variable used to indicate
-		# if the thread should be stopped
-		self.frame = None
-		self.stopped = False
-			def start(self):
-		# start the thread to read frames from the video stream
-		Thread(target=self.update, args=()).start()
-		return self
- 
-	def update(self):
-		# keep looping infinitely until the thread is stopped
-		for f in self.stream:
-			# grab the frame from the stream and clear the stream in
-			# preparation for the next frame
-			self.frame = f.array
-			self.rawCapture.truncate(0)
- 
-			# if the thread indicator variable is set, stop the thread
-			# and resource camera resources
-			if self.stopped:
-				self.stream.close()
-				self.rawCapture.close()
-				self.camera.close()
-				return
-	def read(self):
-		# return the frame most recently read
-		return self.frame
- 
-	def stop(self):
-		# indicate that the thread should be stopped
-		self.stopped = True
+		global mdl_dims
+		def __init__(self, resolution=(mdl_dims, mdl_dims), framerate=32):
+			# initialize the camera and stream
+			self.camera = PiCamera()
+			self.camera.resolution = resolution
+			self.camera.framerate = framerate
+			self.rawCapture = PiRGBArray(self.camera, size=resolution)
+			self.stream = self.camera.capture_continuous(self.rawCapture,
+				format="bgr", use_video_port=True)
+
+			# initialize the frame and the variable used to indicate
+			# if the thread should be stopped
+			self.frame = None
+			self.stopped = False
+				def start(self):
+			# start the thread to read frames from the video stream
+			Thread(target=self.update, args=()).start()
+			return self
+
+		def update(self):
+			# keep looping infinitely until the thread is stopped
+			for f in self.stream:
+				# grab the frame from the stream and clear the stream in
+				# preparation for the next frame
+				self.frame = f.array
+				self.rawCapture.truncate(0)
+
+				# if the thread indicator variable is set, stop the thread
+				# and resource camera resources
+				if self.stopped:
+					self.stream.close()
+					self.rawCapture.close()
+					self.camera.close()
+					return
+		def read(self):
+			# return the frame most recently read
+			return self.frame
+
+		def stop(self):
+			# indicate that the thread should be stopped
+			self.stopped = True
 	
 	cap_stream = PiVideoStream().start()
 	time.sleep(2.0)
