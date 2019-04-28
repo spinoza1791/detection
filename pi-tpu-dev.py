e4faa4c594c3229994c1d53182e26d7c6d12069a
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-27 21:29:24 -0500

    Update pi-tpu-dev.py

--- pi-tpu-dev.py
+++ pi-tpu-dev.py
@@ -67,8 +67,6 @@ def main():
 		cam_res_x=cam_res_y= int(args.cam_res)
 	else:		
 		cam_res_x=cam_res_y= 352		
-
-	img = None
 	
 	class PyCam:
 		def __init__(self, resolution=(320, 320), framerate=32):
@@ -119,19 +117,20 @@ def main():
 			Thread(target=self.update, args=()).start()
 			return self
 		def update(self):
-			global img
-			while img:
-				print("img is not None")
-				self.detect_img = pygame.transform.scale(img,(320,320))
-				self.img_arr = pygame.surfarray.pixels3d(self.detect_img)			
-				self.img_arr = np.swapaxes(self.img_arr,0,1)
-				self.img_arr = np.ascontiguousarray(self.img_arr)
-				self.frame_bytes = io.BytesIO(self.img_arr)
-				self.frame_buf_val = np.frombuffer(self.frame_bytes.getvalue(), dtype=np.uint8)
-				print(self.frame_buf_val)
-				#start_ms = time.time()
-				self.results = self.engine.DetectWithInputTensor(self.frame_buf_val, threshold=0.6, top_k=10)
-				#elapsed_ms = time.time() - start_ms
+			while True:
+				img = pycam_thread.read()	
+				while img:
+					print("img is not None")
+					self.detect_img = pygame.transform.scale(img,(320,320))
+					self.img_arr = pygame.surfarray.pixels3d(self.detect_img)			
+					self.img_arr = np.swapaxes(self.img_arr,0,1)
+					self.img_arr = np.ascontiguousarray(self.img_arr)
+					self.frame_bytes = io.BytesIO(self.img_arr)
+					self.frame_buf_val = np.frombuffer(self.frame_bytes.getvalue(), dtype=np.uint8)
+					print(self.frame_buf_val)
+					#start_ms = time.time()
+					self.results = self.engine.DetectWithInputTensor(self.frame_buf_val, threshold=0.6, top_k=10)
+					#elapsed_ms = time.time() - start_ms
 			if self.stopped:
 				return
 		def read(self):
@@ -162,12 +161,12 @@ def main():
 	#img = pycam.get_image()
 	
 	while True:
-		img = pycam_thread.read()
+		#img = pycam_thread.read()
 		#img = pycam.get_image()
 		#img = pygame.transform.scale(img,(resized_x, resized_y))	
 		#screen.blit(img, (0,0))
-		if img:
-			results = detection_thread.read()
+		#if img:
+		results = detection_thread.read()
 			#detect_img = pygame.transform.scale(img,(mdl_dims,mdl_dims))
 			#img_arr = pygame.surfarray.pixels3d(detect_img)			
 			#img_arr = np.swapaxes(img_arr,0,1)
