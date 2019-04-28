d8f69ca3417090ec2d4d11b6102b23732bc85db3
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-27 20:21:44 -0500

    Update pi-tpu-dev.py

--- pi-tpu-dev.py
+++ pi-tpu-dev.py
@@ -71,11 +71,11 @@ def main():
 		cam_res_x=cam_res_y= 352		
 
 	engine = edgetpu.detection.engine.DetectionEngine(args.model)
-	pygame.init()
-	pygame.camera.init()
 	
 	class PiVideoStream:
 		def __init__(self, resolution=(320, 320), framerate=32):
+			pygame.init()
+			pygame.camera.init()
 			self.screen = pygame.display.set_mode((320, 320), pygame.RESIZABLE)
 			pygame.display.set_caption('Object Detection')
 			self.camlist = pygame.camera.list_cameras()
