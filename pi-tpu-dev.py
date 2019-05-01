349fd4a2473163da8c5109ce4e2e1a43b278dd22
ANDREW CRATON <andrew.craton@gmail.com>
2019-05-01 17:38:17 -0500

    Update pi-tpu-dev.py

--- pi-tpu-dev.py
+++ pi-tpu-dev.py
@@ -176,7 +176,7 @@ def main():
 		screen.blit(img, (0,0))
 
 		#detect_img = pygame.transform.scale(img,(mdl_dims,mdl_dims))
-		img_arr = pygame.surfarray.pixels3d(detect_img)			
+		img_arr = pygame.surfarray.pixels3d(img)			
 		img_arr = np.swapaxes(img_arr,0,1)
 		img_arr = np.ascontiguousarray(img_arr)
 		frame = io.BytesIO(img_arr)
