7b918f86a91bdae552a73170c2a8c2c18875cd09
ANDREW CRATON <andrew.craton@gmail.com>
2019-05-05 22:29:41 -0500

    Update pi-tpu-dev.py

--- pi-tpu-dev.py
+++ pi-tpu-dev.py
@@ -29,9 +29,9 @@ def main():
 	parser.add_argument(
 	  '--video_off', help='Video display on/off, for increased FPS', action='store_true', required=False)
 	parser.add_argument(
-	  '--cam_res_x', help='Set camera X resolution, examples: 96, 128, 256, 352, 384, 480, 640, 1920', default=args.dims, required=False)
+	  '--cam_res_x', help='Set camera X resolution, examples: 96, 128, 256, 352, 384, 480, 640, 1920', default=320, required=False)
 	parser.add_argument(
-	  '--cam_res_y', help='Set camera Y resolution, examples: 96, 128, 256, 352, 384, 480, 640, 1920', default=args.dims, required=False)
+	  '--cam_res_y', help='Set camera Y resolution, examples: 96, 128, 256, 352, 384, 480, 640, 1920', default=320, required=False)
 	if len(sys.argv[0:])==0:
 		parser.print_help()
 		#parser.print_usage() # for just the usage line
