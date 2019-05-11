19b8fa9d1d19e8b70d5c68e10112a92219d060c4
ANDREW CRATON <andrew.craton@gmail.com>
2019-05-11 20:13:39 +0000

    Update pi-tpu.py

--- pi-tpu.py
+++ pi-tpu.py
@@ -193,7 +193,8 @@ def main():
 					fnt_ms_width = fnt_ms.get_rect().width
 					screen.blit(fnt_ms,((resized_x / 2 ) - (fnt_ms_width / 2), 0))
 					bbox_rect = pygame.draw.rect(screen, (0,255,0), (x1, y1, rect_width, rect_height), 4)
-				print(str(fps_avg, class_label, class_score, x1, y1, x2, y2))
+				output = "(%s %s %s %d %d %d %d)" % (fps_avg, class_label, class_score, x1, y1, x2, y2)
+				print(output)
 		else:
 			if not video_off:
 				if i > N:
