9dd3bf160ae8bca641224464bedc5f6ca7f3749b
ANDREW CRATON <andrew.craton@gmail.com>
2019-05-24 15:01:36 -0500

    Update install-ocv4.1.0.sh

--- install-ocv4.1.0.sh
+++ install-ocv4.1.0.sh
@@ -2,8 +2,9 @@
 
 sudo apt-get install -y build-essential git cmake unzip pkg-config && sudo apt-get install -y libjpeg-dev libpng-dev libtiff-dev && sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev && sudo apt-get install -y libxvidcore-dev libx264-dev && sudo apt-get install -y libgtk-3-dev && sudo apt-get install -y libatlas-base-dev gfortran && sudo apt-get install -y python-dev python3-dev python3-pip python-pip python3-setuptools python3-wheel python3-numpy python3-scipy python3-matplotlib && sudo apt-get install -y libeigen2-dev libeigen3-dev libopenexr-dev libgstreamer1.0-dev libgstreamermm-1.0-dev libgoogle-glog-dev libgflags-dev libprotobuf-c-dev libprotobuf-dev protobuf-c-compiler protobuf-compiler
 
-cd ~ && wget -O opencv.zip https://github.com/opencv/opencv/archive/4.1.0.zip && wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.1.0.zip && unzip opencv.zip && unzip opencv_contrib.zip && mv opencv-4.1.0 opencv && mv opencv_contrib-4.1.0 opencv_contrib && cd ~/opencv && mkdir build && cd build
+//cd ~ && wget -O opencv.zip https://github.com/opencv/opencv/archive/4.1.0.zip && wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.1.0.zip && unzip opencv.zip && unzip opencv_contrib.zip && mv opencv-4.1.0 opencv && mv opencv_contrib-4.1.0 opencv_contrib
 
+cd ~/opencv && mkdir build && cd build
 
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
@@ -22,4 +23,4 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D PYTHON_DEFAULT_EXECUTABLE=$(which python3) \
     -D BUILD_EXAMPLES=OFF ..
 
- make -j4 && sudo make install & sudo ldconfig
+cd ~/opencv/build && make -j4 && sudo make install & sudo ldconfig
